3056c7b08afbd93100893bf8ea8627f23be017ee
diwasbk2k3 <diwasbk2k3@gmail.com>
2025-12-29 10:54:25 +0545

    Batch Repository

---
+++ lib/features/auth/domain/repositories/batch_repository.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:lost_n_found/core/error/failures.dart';
+import 'package:lost_n_found/features/batch/domain/entities/batch_entity.dart';
+
+abstract class BatchRepository {
+  Future<Either<Failure, List<BatchEntity>>> getAllBatches();
+  Future<Either<Failure, BatchEntity>> getBatchById();
+  Future<Either<Failure, bool>> createBatch(BatchEntity entity);
+  Future<Either<Failure, bool>> updateBatch(BatchEntity entity);
+  Future<Either<Failure, bool>> deleteBatch(String batchId);
+}
