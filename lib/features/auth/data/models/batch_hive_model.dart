bf8399ba1917aed91204a92830589516c8ec7331
diwasbk2k3 <diwasbk2k3@gmail.com>
2025-12-29 22:26:32 +0545

    Batch Hive Model

---
+++ lib/features/auth/data/models/batch_hive_model.dart
@@ -0,0 +1,34 @@
+import 'package:hive/hive.dart';
+import 'package:lost_n_found/core/constants/hive_table_constant.dart';
+import 'package:lost_n_found/features/auth/domain/entities/batch_entity.dart';
+import 'package:uuid/uuid.dart';
+
+@HiveType(typeId: HiveTableConstant.batchTypeId)
+class BatchHiveModel extends HiveObject {
+  @HiveField(0)
+  final String? batchId;
+
+  @HiveField(1)
+  final String? batchName;
+
+  @HiveField(2)
+  final String? status;
+
+  BatchHiveModel({String? batchId, required this.batchName, String? status})
+    : batchId = batchId ?? const Uuid().v4(),
+      status = status ?? 'active';
+
+  BatchEntity toEntity() {
+    return BatchEntity(batchId: batchId, batchName: batchName, status: status);
+  }
+
+  // from Entity
+  factory BatchHiveModel.fromEntity(BatchEntity entity) {
+    return BatchHiveModel(batchName: entity.batchName);
+  }
+
+  // from Entity List
+  static List<BatchEntity> toEntityList(List<BatchHiveModel> models) {
+    return models.map((model) => model.toEntity()).toList();
+  }
+}
