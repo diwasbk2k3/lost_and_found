fb256b79cfd62f4261acbef3eb1500915c4398bf
diwasbk2k3 <diwasbk2k3@gmail.com>
2026-01-01 01:05:08 +0545

    Auth View Model with Provider Implementation

---
+++ lib/features/auth/presentation/view_model/auth_view_model.dart
@@ -0,0 +1,74 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lost_n_found/features/auth/domain/usecases/login_usecase.dart';
+import 'package:lost_n_found/features/auth/domain/usecases/register_usecase.dart';
+import 'package:lost_n_found/features/auth/presentation/state/auth_state.dart';
+
+// Provider implementation for AuthViewModel
+final authViewModelProvider = NotifierProvider<AuthViewModel, AuthState>(
+  () => AuthViewModel(),
+);
+
+class AuthViewModel extends Notifier<AuthState> {
+  late final RegisterUsecase _registerUsecase;
+  late final LoginUsecase _loginUsecase;
+
+  @override
+  AuthState build() {
+    _registerUsecase = ref.read(registerUsecaseProvider);
+    _loginUsecase = ref.read(loginUsecaseProvider);
+    return const AuthState();
+  }
+
+  // Register method
+  Future<void> register({
+    required String fullName,
+    required String email,
+    String? phoneNumber,
+    String? batchId,
+    required String username,
+    required String password,
+  }) async {
+    state = state.copyWith(status: AuthStatus.loading);
+    final params = RegisterUsecaseParams(
+      fullName: fullName,
+      email: email,
+      phoneNumber: phoneNumber,
+      batchId: batchId,
+      username: username,
+      password: password,
+    );
+    final result = await _registerUsecase(params);
+    result.fold(
+      (failure) {
+        state = state.copyWith(
+          status: AuthStatus.error,
+          errorMessage: failure.message,
+        );
+      },
+      (authEntity) {
+        state = state.copyWith(status: AuthStatus.registered);
+      },
+    );
+  }
+
+  // Login method
+  Future<void> login({required String email, required String password}) async {
+    state = state.copyWith(status: AuthStatus.loading);
+    final params = LoginUseCaseParams(email: email, password: password);
+    final result = await _loginUsecase(params);
+    result.fold(
+      (failure) {
+        state = state.copyWith(
+          status: AuthStatus.error,
+          errorMessage: failure.message,
+        );
+      },
+      (authEntity) {
+        state = state.copyWith(
+          status: AuthStatus.authenticated,
+          authEntity: authEntity,
+        );
+      },
+    );
+  }
+}
