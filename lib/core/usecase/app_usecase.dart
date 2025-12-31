1927c7a436fafb5b3bd0dd62d348e269b9fc1f07
diwasbk2k3 <diwasbk2k3@gmail.com>
2025-12-31 16:10:32 +0545

    Use Case With Params and Without Params

---
+++ lib/core/usecase/app_usecase.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:lost_n_found/core/error/failures.dart';
+
+abstract interface class UseCaseWithParams<SuccessType, Params> {
+  Future<Either<Failure, SuccessType>> createBatch(Params params);
+}
+
+abstract interface class UseCaseWithoutParams<SuccessType> {
+  Future<Either<Failure, SuccessType>> createBatch();
+}
