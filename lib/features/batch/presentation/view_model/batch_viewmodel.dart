dbb3dfb40be449c9824e7b5a10478213881dc32f
diwasbk2k3 <diwasbk2k3@gmail.com>
2025-12-31 17:50:19 +0545

    Batch View Model

---
+++ lib/features/batch/presentation/view_model/batch_viewmodel.dart
@@ -0,0 +1,57 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lost_n_found/features/batch/domain/usecases/create_batch_usecase.dart';
+import 'package:lost_n_found/features/batch/domain/usecases/get_all_batch_usecase.dart';
+import 'package:lost_n_found/features/batch/domain/usecases/update_batch_usecase.dart';
+import 'package:lost_n_found/features/batch/presentation/state/batch_state.dart';
+
+class BatchViewmodel extends Notifier<BatchState> {
+  late final GetAllBatchUsecase _getAllBatchUsecase;
+  late final UpdateBatchUsecase _updateBatchUsecase;
+  late final CreateBatchUsecase _createBatchUsecase;
+
+  @override
+  BatchState build() {
+    // initialization
+    return BatchState();
+  }
+
+  // Get All Batch
+  Future<void> getAllBatches() async {
+    state = state.copyWith(status: BatchStatus.loading);
+    final result = await _getAllBatchUsecase();
+
+    result.fold(
+      (left) {
+        state = state.copyWith(
+          status: BatchStatus.error,
+          errorMessage: left.message,
+        );
+      },
+      (batches) {
+        state = state.copyWith(status: BatchStatus.loaded, batches: batches);
+      },
+    );
+  }
+
+  // Create Batch
+  Future<void> createBatch(String batchName) async {
+    // progress bar lai ghumaune
+    state = state.copyWith(status: BatchStatus.loading);
+    final result = await _createBatchUsecase(
+      CreateBatchUsecaseParams(batchName: batchName),
+    );
+
+    result.fold(
+      (left) {
+        state = state.copyWith(
+          status: BatchStatus.error,
+          errorMessage: left.message,
+        );
+      },
+      (right) {
+        state = state.copyWith(status: BatchStatus.loaded);
+      },
+    );
+  }
+}
